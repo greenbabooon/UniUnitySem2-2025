e4d279bac43e95e899434258ab2d6e1876d89bc5
Grolsch1 <chrisbeckenstrater@gmail.com>
2025-11-09 17:48:00 +0200

    Fixes to UIs and Scene loading

--- Group21-ZombieGame/Assets/Scripts/Playerstuffs/PlayerController.cs
+++ Group21-ZombieGame/Assets/Scripts/Playerstuffs/PlayerController.cs
@@ -96,6 +96,7 @@ public class PlayerController : MonoBehaviour, IDamageable
         Invoke("delayedReset", 0.1f);
 
         GameManager.gameManager.SetCurrentPlayerController(this);
+        GameManager.gameManager.CurrentLevel();
     }
     private void OnEnable()
     {
@@ -507,9 +508,8 @@ public class PlayerController : MonoBehaviour, IDamageable
         
         if (healthScript.currentHealth <= 0)
         {
-            //improve death logic
             playerAnimController.deathSFX();
-            SceneManager.LoadScene("Death Screen");
+            GameManager.gameManager.Death();
         }
     }
     void damageAlertCancel()
